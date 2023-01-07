0a1a976595ee13341e04bf81bdf9fe0c4f8b21f4
rum <rumstxck@gmail.com>
2023-01-07 14:19:28 +0100

    Fix 404 page

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -2,14 +2,14 @@ import './NotFound.style.scss'
 
 const NotFound = () => {
   const img404: string[] = [
-    "https://im.ezgif.com/tmp/ezgif-1-6847bdd35b.webp",
+    "/assets/404/lavarestformelle.webp",
     "https://img-9gag-fun.9cache.com/photo/a8qynQV_700bwp.webp",
-    "https://im.ezgif.com/tmp/ezgif-1-28ba37d077.webp",
+    "assets/404/jpn_backto_ger.webp",
     "https://media.tenor.com/7gQkxPeaocYAAAAd/mbappe-mbappe-laugh.gif",
     "https://media.tenor.com/WeR1fN4iik8AAAAd/neymar-neymar-jr.gif",
     "https://media.tenor.com/WJbXIiTBusYAAAAC/vini-neymar-paqueta-dance.gif",
-    "https://im.ezgif.com/tmp/ezgif-1-ad40fec23c.gif",
-    "https://im.ezgif.com/tmp/ezgif-1-1d2db58c0e.gif"
+    "https://media.tenor.com/lpy9YSehZA4AAAAC/wheresmessi-messi.gif",
+    "https://media.tenor.com/fvEBqAU209oAAAAC/harry-kane-penalty-miss.gif"
   ]
 
   const min: number = 0;
